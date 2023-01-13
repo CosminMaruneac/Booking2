99adcf871d9e2761ae445657b754443bbdec08de
Cosmin Maruneac <cosmin.maruneac@assist.ro>
2023-01-13 17:06:21 +0200

    Added create room and Swagger

--- src/main/java/com/usv/booking/features/room/RoomController.java
+++ src/main/java/com/usv/booking/features/room/RoomController.java
@@ -3,6 +3,7 @@ package com.usv.booking.features.room;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class RoomController {
   private final RoomService roomService;
 
   @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public RoomDto create(RoomDto dto){
+  public RoomDto create(@RequestBody RoomDto dto){
 
     return roomService.create(dto);
   }
